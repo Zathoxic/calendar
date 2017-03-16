06e7d254bde260733e65d0c90e07b1d2306f5964
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-03-16 11:12:28 +0100

    Added prefix 0 for months

--- insert.php
+++ insert.php
@@ -8,6 +8,12 @@ $day = $_POST['day'];
 $month = $_POST['month'];
 $year = $_POST['year'];
 
+$monthPrefix = 0;
+
+if(strlen($month) < 2 && strlen($month) > 0){
+    $month = $monthPrefix . $month;
+}
+
 $query = "INSERT INTO birthdays (person, day, month, year) VALUES ('$person', '$day', '$month', '$year')";
 
 var_dump($query);
--- update.php
+++ update.php
@@ -7,6 +7,12 @@ $day = $_POST['day'];
 $month = $_POST['month'];
 $year = $_POST['year'];
 
+$monthPrefix = 0;
+
+if(strlen($month) < 2 && strlen($month) > 0){
+    $month = $monthPrefix . $month;
+}
+
 $query = "UPDATE birthdays SET person='$person', day='$day', month='$month', year='$year' WHERE id=$id";
 
 if (!mysqli_query($conn, $query)){
